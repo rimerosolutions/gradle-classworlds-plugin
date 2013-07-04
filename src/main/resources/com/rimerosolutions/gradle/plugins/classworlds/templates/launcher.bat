f3a57c109f75f5c680e4cc052041a879e4f80ae1
unknown <yzoundi@M00030105.mackenziefinancial.bz>
2013-07-04 13:08:55 -0400

    Updating broken launchers code

--- src/main/resources/com/rimerosolutions/gradle/plugins/classworlds/templates/launcher.bat
+++ src/main/resources/com/rimerosolutions/gradle/plugins/classworlds/templates/launcher.bat
@@ -1,2 +1,9 @@
-${shellComments}
+${shellHeader}
+${shellComment}
+@echo off
+@setlocal
+@setlocal enableextensions enabledelayedexpansion
+for /f %%i in ("%0") do set USER_INSTALL=%%~dpi
+set ${appHome}=%USER_INSTALL%\\..
+
 java -classpath %${appHome}%\\boot\\${bootJarFileName} -Dclassworlds.conf=%${appHome}%\\etc\\classworlds.conf -Dapp.home=%${appHome}% org.codehaus.plexus.classworlds.launcher.Launcher %*
--- src/main/resources/com/rimerosolutions/gradle/plugins/classworlds/templates/launcher.sh
+++ src/main/resources/com/rimerosolutions/gradle/plugins/classworlds/templates/launcher.sh
@@ -1,3 +1,6 @@
 ${shellHeader}
-${shellComments}
+${shellComment}
+DIRNAME=\$(dirname \$BASH_SOURCE)
+USER_INSTALL=\$(cd \$DIRNAME/.. && pwd)
+export ${appHome}=\${USER_INSTALL}
 java -classpath \${$appHome}/boot/${bootJarFileName} -Dclassworlds.conf=\${$appHome}/etc/classworlds.conf -Dapp.home=\${$appHome} org.codehaus.plexus.classworlds.launcher.Launcher \$*
